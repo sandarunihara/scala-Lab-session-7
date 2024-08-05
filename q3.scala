72196487d14d1ed0d7a10ec7eca2aef071641be8
sandarunihara <sandarunihara56@gmail.com>
2024-08-05 20:48:16 +0530

    3

--- q3.scala
+++ q3.scala
@@ -10,7 +10,6 @@
   def main(args: Array[String]): Unit = {
     val inputList = List(1,2,3,4,5,6,7,13,15,17,23)
     val primeNumbers = filterPrime(inputList)
-    
     println(s"Input List: $inputList")
 
     if(primeNumbers.isEmpty){
