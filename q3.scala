f8d7cfdd1ff666157c66e2b7bbfc7d30702a6e89
sandarunihara <sandarunihara56@gmail.com>
2024-08-05 20:27:24 +0530

    3

--- q3.scala
+++ q3.scala
@@ -3,18 +3,14 @@
     else (2 until num).forall(i => num % i != 0)
   }
 
-  // Function to filter prime numbers from a list
   def filterPrime(numbers: List[Int]): List[Int] = {
-    // Use the filter method with a lambda function to select prime numbers
     numbers.filter(num => isPrime(num))
   }
 
   def main(args: Array[String]): Unit = {
-    // Example input list
     val inputList = List(1,2,3,4,5,6,7,13,15,17,23)
     val primeNumbers = filterPrime(inputList)
     
-    // Print the result
     println(s"Input List: $inputList")
 
     if(primeNumbers.isEmpty){
