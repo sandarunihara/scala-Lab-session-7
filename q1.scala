0789372b558d12e35a65ab4b3c5d388a851bed2f
sandarunihara <sandarunihara56@gmail.com>
2024-08-05 19:57:30 +0530

    1

--- q1.scala
+++ q1.scala
@@ -2,12 +2,10 @@ def filterEvenNumbers(numbers: List[Int]): List[Int] = {
   numbers.filter(num => num % 2 == 0)
 }
 
-var even=(num:List[Int])=>{
-  num.filter(num=>num%2==0)
-}
+
 def main(args: Array[String]): Unit = {
 val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-val output = even(input)
+val output = filterEvenNumbers(input)
 println(output)  
 }
 
